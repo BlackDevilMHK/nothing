b0e41d79f03faed20c9a34961ea2f21f9ccb6a44
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 17:36:35 +0600

    Update and rename ntg.py to ntg.sh

--- ntg.py
+++
@@ -1,8 +0,0 @@
-# Enter Your Basic Info
-name=input("$ Enter Your Name : ")
-cname=input("$ Enter Your CodeName : ")
-age=input("$ Enter Your Age : ")
-address=input("$ Enter Your Address : ")
-team=input("$ Enter Your TeamName : ")
-
-print(" TNX FOR YOUR INFO")
---
+++ ntg.sh
@@ -0,0 +1,8 @@
+echo 'Enter Your Some Info'|lolcat
+echo 'name=input("$ Enter Your Name : ")'|lolcat
+echo 'cname=input("$ Enter Your CodeName : ")'|lolcat
+echo 'age=input("$ Enter Your Age : ")'|lolcat
+echo 'address=input("$ Enter Your Address : ")'|lolcat
+echo 'team=input("$ Enter Your TeamName : ")'|lolcat
+
+echo ' Thunku For Your Info'|lolcat
