bdc4599dbf15bee03b1a0fea56c8d180211df875
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 17:38:19 +0600

    Delete ntg.sh

--- ntg.sh
+++
@@ -1,8 +0,0 @@
-echo 'Enter Your Some Info'|lolcat
-echo 'name=input("$ Enter Your Name : ")'|lolcat
-echo 'cname=input("$ Enter Your CodeName : ")'|lolcat
-echo 'age=input("$ Enter Your Age : ")'|lolcat
-echo 'address=input("$ Enter Your Address : ")'|lolcat
-echo 'team=input("$ Enter Your TeamName : ")'|lolcat
-
-echo ' Thunku For Your Info'|lolcat
