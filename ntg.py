d38e2c4fc5219bd275ee9c1dd242b77788e47d98
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 17:40:16 +0600

    Create ntg.py

---
+++ ntg.py
@@ -0,0 +1,15 @@
+# Enter Your Basic Info
+print (" ")
+print (" ")
+name=input("$ Enter Your Name : ")
+
+print (" ")
+cname=input("$ Enter Your CodeName : ")
+print (" ")
+age=input("$ Enter Your Age : ")
+print (" ")
+address=input("$ Enter Your Address : ")
+print (" ")
+team=input("$ Enter Your TeamName : ")
+
+print(" TNX FOR YOUR INFO")
